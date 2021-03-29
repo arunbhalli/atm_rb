80b6fd07f94745293f8cb5718f846d674f4f9f33
arunbhalli <tomarbhalli.com>
2021-03-29 19:44:11 +0200

    all methods and codding is working

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -4,4 +4,14 @@ describe Atm do
   it "is expected to be an instance of Atm " do
     expect(subject.class).to equal Atm
   end
+  it "is expected to be an instance of Atm " do
+    expect(subject.class).to equal Atm
+  end
+  it "is expected to hold 1000 cur an initialize" do
+    expect(subject.funds).to equal 1000
+  end
+  it "is expected to hold 1000 cur an initialize" do
+    subject.withdraw(150)
+    expect(subject.funds).to equal 850
+  end
 end
